a82ad269119f14bdd253935a49a02743d8da66d0
menglb <lingbinmeng@hotmail.com>
2017-03-13 13:33:17 +0800

    添加主题功能，完善注释。

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pinyin
-把汉字转化为拼音字母，由chinalu/JSPinyin改写，使用新语法并去掉mootools的依赖
+把汉字转化为拼音字母，由[chinalu/JSPinyin](https://github.com/chinalu/JSPinyin)改写，使用新语法并去掉mootools的依赖
---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @fileOverview 导出默认的实例
+ * @author waterchestnut 2017/3/13
+ * @module
+ */
+'use strict';
+
+let pinyin = new (require('./pinyin'))({charCase:2});
+/*console.log(pinyin.getFullChars('管理员'))
+console.log(pinyin.getCamelChars('管理员'))
+console.log(pinyin.getCamelChars('1234'))
+console.log(pinyin.getCamelChars('english'))*/
+module.exports = pinyin;
