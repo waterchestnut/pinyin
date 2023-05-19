4b90afb352628b2500b5ba7700e046b5a062501c
xianyang <lingbinmeng@hotmail.com>
2023-05-19 18:58:24 +0800

    修复佛读音的错误。

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
 'use strict';
 
 let pinyin = new (require('./dist/pinyin'))({charCase:0});
-/*console.log(pinyin.getFullChars('管理员'))
+pinyin.setOptions({checkPolyphone: true, charCase: 0});
+console.log(pinyin.getFullChars('管理员'))
 console.log(pinyin.getCamelChars('管理员'))
 console.log(pinyin.getCamelChars('1234'))
 console.log(pinyin.getCamelChars('english'))
-console.log(pinyin.getCamelChars('昕'))*/
+console.log(pinyin.getCamelChars('昕'))
+console.log(pinyin.getCamelChars('佛'))
+console.log(pinyin.getFullChars('佛'))
 module.exports = pinyin;
