9450c7f17dd346f5b1202446bc258cda0854fc31
xianyang <lingbinmeng@hotmail.com>
2023-05-19 19:00:50 +0800

    修改版本号。

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
 'use strict';
 
 let pinyin = new (require('./dist/pinyin'))({charCase:0});
-pinyin.setOptions({checkPolyphone: true, charCase: 0});
+/*pinyin.setOptions({checkPolyphone: true, charCase: 0});
 console.log(pinyin.getFullChars('管理员'))
 console.log(pinyin.getCamelChars('管理员'))
 console.log(pinyin.getCamelChars('1234'))
 console.log(pinyin.getCamelChars('english'))
 console.log(pinyin.getCamelChars('昕'))
 console.log(pinyin.getCamelChars('佛'))
-console.log(pinyin.getFullChars('佛'))
+console.log(pinyin.getFullChars('佛'))*/
 module.exports = pinyin;
